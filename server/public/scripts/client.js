0186416ec9a09e7c7e04984703309e62d3c91754
Gabriel Regalado <regs0404@gmail.com>
2023-12-24 16:02:44 -0500

    working on client side: have the routes to receive data however need to update code in order to take in the operator buttons to perform calculations

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,7 +15,8 @@ function getCalculations() {
       url: '/calculations',
     })
       .then(function (response) {
-        renderCalculations(response.data);
+        let newCalculation = response.data
+        renderCalculations(newCalculation);
       })
       .catch(function (error) {
         console.log('Error getting calculations', error);
@@ -24,23 +25,50 @@ function getCalculations() {
   }
 
 
-  function renderCalculations(newCalc) {
-    console.log('rendering calculations to the DOM', newCalc);
+  function renderCalculations(newCalculation) {
+    console.log('rendering calculations to the DOM', newCalculation);
   
-    let calculationsResults = document.getElementById('calculationsResults');
+    let recentResult = document.getElementById('recentResult');
+    let resultHistory = document.getElementById('resultHistory')
   
-    // empty the output element
-    calculationsResults.innerHTML = '';
-  
+    // empty the output element for recent results
+    recentResult.innerHTML = '';
+ 
     // loop through the books to display them
-    for (let item of newCalc) {
-      // Append the book to the DOM in book output area
-      calculationsResults.innerHTML += `
+    for (let item of newCalculation) {
+      // Append the History to the DOM in history output area
+      resultHistory.innerHTML += `
             <li>
-              <p>${item}</p>
+              <p>${item.numOne}${item.operator}${item.numTwo} = ${item.result}</p>
             </li>`;
+    } 
+    // Display recent results to the recent section
+    if (newCalculation.length > 0) {
+        recentResult.innerHTML += `
+        <li>${newCalculation[newCalculation.length - 1].result}</li>`;
     }
+}
+
+function addCalculation(event) {
+    // Store the calculations inputs in variables
+    let firstNumber = document.getElementById('firstNumber');
+    let secondNumber = document.getElementById('secondNumber');
+  
+    // Send the new data to the server
+    axios({
+      method: 'POST',
+      url: '/calculations',
+      data: newCalculation,
+    }).then(function (response) {
+      console.log('Calculation added');
+  
+      // Clear the inputs
+      firstNumber.value = '';
+      secondNumber.value = '';
+
+    });
   }
+
 
 
 
